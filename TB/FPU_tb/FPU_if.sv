12dd8ada67813154ebc93bf8e2dbf9b9aa1dcd37
OsamaMohamed666 <85251357+OsamaMohamed666@users.noreply.github.com>
2025-10-23 01:58:16 +0300

    Adding some assertions for FPU

--- TB/FPU_tb/FPU_if.sv
+++ TB/FPU_tb/FPU_if.sv
@@ -1,3 +1,6 @@
+`include "uvm_macros.svh"
+import uvm_pkg::*;
+
 interface fpu_if (input bit clk); //, bit rstn);
 
 bit rstn;
@@ -31,7 +34,64 @@ clocking cb @(posedge clk);
   input Result;
 endclocking
 
-// Modport for driver
+//Modport for driver
 modport driver_mp (clocking cb, input clk, output rstn);
+
+
+//ASSERTIONS
+
+//1) Reset checking
+property reset_chk;
+  @(posedge clk)
+  !rstn |-> (Result==32'b0);
+endproperty
+
+assert property(reset_chk)
+  else `uvm_error("ASSERTIONS" ," RESET STATE IS VIOLATED")
+reset_chk_p1: cover property (reset_chk);
+
+//2) Checking not a number flag is hitted by op1 and op2
+property op1_op2_NAN;
+  @(negedge clk) disable iff  (!rstn)  (OP_select ==0 || OP_select ==1 || OP_select ==2)
+  |-> ($rose(OP1[30:23]==8'hff) || $rose(OP2[30:23]==8'hff)) ##0 ($rose(OP1[22:0] !=0) || $rose(OP1[22:0] !=0))
+  |-> NAN_flag;
+endproperty
+
+assert property(op1_op2_NAN)
+  else `uvm_error("ASSERTIONS" ," OPERANDS NOT A NUMBER CHECKING IS VIOLATED");
+op1_op2_NAN_p2: cover property (op1_op2_NAN);
+
+// //3) Checking infinite flag is hitted by op1 and op2
+// property op1_op2_INF;
+//   @(negedge clk) disable iff  (!rstn)  (OP_select ==0 || OP_select ==1 || OP_select ==2)
+//   |-> ($rose(OP1[30:23]==8'hff) || $rose(OP2[30:23]==8'hff)) ##0 ($rose(OP1[22:0] ==0) || $rose(OP1[22:0] ==0))
+//   ##0 !NAN_flag |-> INF_flag; // checking that op1 or op2 isnot a nan as nan is the dominant flag
+// endproperty
+
+// assert property(op1_op2_INF)
+//   else `uvm_error("ASSERTIONS" ," INFINITE OPERANDS CHECKING IS VIOLATED");
+// op1_op2_INF_p3: cover property (op1_op2_INF) $display("The prop property 3 is hit");
+
+//3) Checking not a number flag is hitted by Result
+property result_NAN;
+  @(negedge clk) disable iff  (!rstn)
+  $rose(Result[30:23]==8'hff) ##0 $rose(Result[22:0] !=0)
+  |-> NAN_flag;
+endproperty
+  assert property(result_NAN)
+  else `uvm_error("ASSERTIONS" ," RESULT NOT A NUMBER CHECKING IS VIOLATED");
+result_NAN_p4: cover property(result_NAN);
+
+//4) Checking INF flag is hitted by Result
+property result_INF;
+  @(negedge clk) disable iff  (!rstn)
+  $rose(Result[30:23]==8'hff) ##0 $rose(Result[22:0] == 23'b0)
+  |-> INF_flag;
+endproperty
+
+assert property(result_INF)
+  else `uvm_error("ASSERTIONS" ," RESULT INFINITE  CHECKING IS VIOLATED");
+result_INF_p5: cover property(result_INF);
+
 
 endinterface
